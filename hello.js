2df654e6d4fdc515e665b131b752205aa2e9a1e6
Mauricio Barón <diegombaron@gmail.com>
2023-05-11 22:22:37 -0500

    Ejercicios terminados

---
+++ hello.js
@@ -0,0 +1,8 @@
+// escribe la función hola acá
+function hola(nombre){
+    console.log("Hola " + nombre + '!');
+}
+// código de prueba
+console.log(hola("Pedro")) // "Hola Pedro!"
+console.log(hola("Juan")) // "Hola Juan!"
+console.log(hola("Mao")) // "Hola !"
---
+++ sum.js
@@ -0,0 +1,11 @@
+// escribe la función suma acá
+function sum(num){
+  for (let i = 0; i <= num; i++) {
+    console.log(i);
+    
+  }
+}
+  // código de prueba
+console.log(sum(4)) // 1 + 2 + 3 + 4 = 10
+console.log(sum(10)) // 55
+console.log(sum(15)) // 120
